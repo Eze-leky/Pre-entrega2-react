a9e849d5a9dc957ac2a90d4be8f44ff010547e45
Eze-leky <ezequielmorales232@gmail.com>
2023-03-21 12:00:04 -0300

    acomodos

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -6,27 +6,26 @@ import ItemList from "../itemList/ItemList";
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    const productList = new Promise((resolve, reject) => {
+      resolve(products);
+    });
+
+    productList
+      .then((res) => {
+        setItems(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  // console.log(items);
+
   return (
-    <div>
-      <h1> hola</h1>
+    <div className>
       <ItemList items={items} />
     </div>
   );
 };
 
 export default ItemListContainer;
-// useEffect(() => {
-//   const productList = new Promise((resolve, reject) => {
-//     resolve(products);
-//     // reject("algo salio mal");
-//   });
-
-//   productList
-//     .then((res) => {
-//       setItems(res);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// }, []);
-// console.log(items);
